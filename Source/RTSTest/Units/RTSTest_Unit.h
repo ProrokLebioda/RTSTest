0e8cb381d213deaa64a2342296c5f5ca50a2ce9a
DESKTOP-99HRA0O\Patryk <komosa.patryk@gmail.com>
2023-02-17 23:26:40 +0100

    Added child BP for buildings

--- Source/RTSTest/Units/RTSTest_Unit.h
+++ Source/RTSTest/Units/RTSTest_Unit.h
@@ -22,13 +22,12 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* MeshComponent;
-
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* MeshComponent;
 
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
